1839f62d3422a62d7c7dd701d936d536e18b6eaa
Yoann Chhang <yoann.chhang@gmail.com>
2021-12-26 20:49:15 +0100

    path

--- score.py
+++ score.py
@@ -1,9 +1,9 @@
 def getHighScore():
-    with open("./1PYTH2/score.txt", 'r') as file:
+    with open("./score.txt", 'r') as file:
         lines = file.readlines()
         return lines[0]
 
 
 def setScore(score):
-    with open("./1PYTH2/score.txt", 'w') as file:
+    with open("./score.txt", 'w') as file:
         file.write(str(score))
